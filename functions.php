588d95b7d152e12bcf395d958a3d92d13d26a4e2
19h47 <jeremylevron@19h47.fr>
2022-11-16 15:29:15 +0100

    :tada: Initial commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Les Rapporteuses functions and definitions
+ *
+ * @package WordPress
+ * @subpackage LesRapporteuses
+ */
+
+// Autoloader.
+// require_once get_template_directory() . '/vendor/autoload.php';
+
+// Timber\Timber::init();
+// CreatorFund\Init::run_services();
